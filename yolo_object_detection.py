393a8bc6975b98d9c748fd6fd14c6c6bf76fb9d7
DNS-0522 <xkoukix111@gmail.com>
2021-11-07 23:41:52 +0800

    Improved readability

--- yolo_object_detection.py
+++ yolo_object_detection.py
@@ -18,6 +18,39 @@ class yolo_object_detection():
     __confidence_setting = 0.5
     __threshold = 0.2
 
+    def __judge_object_confidence_label_to_get_bbox(self, layerOutputs, boxes, confidences, crop_width, crop_height, crop_x, crop_y, classIDs):
+        for output in layerOutputs:
+            # loop over each of the detections
+            for detection in output:
+                # extract the class ID and confidence (i.e., probability)
+                # of the current object detection
+                scores = detection[5:]
+                classID = np.argmax(scores)
+                confidence = scores[classID]
+                if self.__LABELS[classID] !=  self.__target_label:
+                    continue
+
+               # filter out weak predictions by ensuring the detected
+                # probability is greater than the minimum probability
+                if confidence > self.__confidence_setting:
+                    # scale the bounding box coordinates back relative to
+                    # the size of the image, keeping in mind that YOLO
+                    # actually returns the center (x, y)-coordinates of
+                    # the bounding box followed by the boxes' width and
+                    # height
+                    box = detection[0:4] * np.array([crop_width, crop_height, crop_width, crop_height])
+                    (centerX, centerY, width, height) = box.astype("int")
+                    if width < crop_width * 2 / 3:
+                        # use the center (x, y)-coordinates to derive the top
+                        # and and left corner of the bounding box
+                        x = int(centerX - (width / 2))
+                        y = int(centerY - (height / 2))
+
+                        # update our list of bounding box coordinates,
+                        # confidences, and class IDs
+                        boxes.append([crop_x + x, crop_y + y, int(width), int(height)])
+                        confidences.append(float(confidence))
+                        classIDs.append(classID)
     # public
     def __init__(self, target_label):
         np.random.seed(42)
@@ -45,96 +78,46 @@ class yolo_object_detection():
         confidences = []
         classIDs = []
         crop_width = 1920
-        crop_high = 1080
+        crop_height = 1080
         # construct a blob from the input frame and then perform a forward
         # pass of the YOLO object detector, giving us our bounding boxes
         # and associated probabilities
         # crop small area to improve the effect of yolo
-        # crop image size 960*540
-        # overlap 40%
+        # crop image size 1920*920
+        # overlap 50%
         for crop_x in range(0, 2840, 960):
             for crop_y in range(0, 1620, 540):
                 # print("crop_y:%d" % crop_y)
-                # print("crop_y + crop_high:%d" % (crop_y + crop_high))
+                # print("crop_y + crop_height:%d" % (crop_y + crop_height))
                 # print("crop_x:%d" % crop_x)
                 # print("crop_x + crop_width:%d" % (crop_x + crop_width))
 
-                crop_img = frame[crop_y:crop_y + crop_high, crop_x:crop_x + crop_width]
+                crop_img = frame[crop_y:crop_y + crop_height, crop_x:crop_x + crop_width]
                 blob = cv2.dnn.blobFromImage(crop_img, 1 / 255.0, (416, 416),
                                              swapRB=True, crop=False)
                 self.__net.setInput(blob)
                 layerOutputs = self.__net.forward(ln)
                 # loop over each of the layer outputs
-                for output in layerOutputs:
-                    # loop over each of the detections
-                    for detection in output:
-                        # extract the class ID and confidence (i.e., probability)
-                        # of the current object detection
-                        scores = detection[5:]
-                        classID = np.argmax(scores)
-                        confidence = scores[classID]
+                self.__judge_object_confidence_label_to_get_bbox(layerOutputs, boxes, confidences,crop_width, crop_height, crop_x,
+                                                                 crop_y, classIDs)
 
-                        # filter out weak predictions by ensuring the detected
-                        # probability is greater than the minimum probability
-                        if confidence > self.__confidence_setting:
-                            # scale the bounding box coordinates back relative to
-                            # the size of the image, keeping in mind that YOLO
-                            # actually returns the center (x, y)-coordinates of
-                            # the bounding box followed by the boxes' width and
-                            # height
-                            box = detection[0:4] * np.array([crop_width, crop_high, crop_width, crop_high])
-                            (centerX, centerY, width, height) = box.astype("int")
-                            if width < crop_width * 2 / 3:
-                                # use the center (x, y)-coordinates to derive the top
-                                # and and left corner of the bounding box
-                                x = int(centerX - (width / 2))
-                                y = int(centerY - (height / 2))
-
-                                # update our list of bounding box coordinates,
-                                # confidences, and class IDs
-                                boxes.append([crop_x + x, crop_y + y, int(width), int(height)])
-                                confidences.append(float(confidence))
-                                classIDs.append(classID)
+            # full image
         blob = cv2.dnn.blobFromImage(frame, 1 / 255.0, (416, 416),
                                      swapRB=True, crop=False)
         self.__net.setInput(blob)
         layerOutputs = self.__net.forward(ln)
-        # initialize our lists of detected bounding boxes, confidences,
-        # and class IDs, respectively
-        # loop over each of the layer outputs
-        for output in layerOutputs:
-            # loop over each of the detections
-            for detection in output:
-                # extract the class ID and confidence (i.e., probability)
-                # of the current object detection
-                scores = detection[5:]
-                classID = np.argmax(scores)
-                confidence = scores[classID]
+        self.__judge_object_confidence_label_to_get_bbox(layerOutputs, boxes, confidences,0, 0, 0, 0, classIDs)
 
-                # filter out weak predictions by ensuring the detected
-                # probability is greater than the minimum probability
-                if confidence > self.__confidence_setting:
-                    # scale the bounding box coordinates back relative to
-                    # the size of the image, keeping in mind that YOLO
-                    # actually returns the center (x, y)-coordinates of
-                    # the bounding box followed by the boxes' width and
-                    # height
-                    box = detection[0:4] * np.array([W, H, W, H])
-                    (centerX, centerY, width, height) = box.astype("int")
+        output_image_sw=True
+        if output_image_sw == True:
+            out_frame = frame.copy()
+            for i, bbox in enumerate(boxes):
+                (x, y) = (bbox[0], bbox[1])
+                (w, h) = (bbox[2], bbox[3])
+                color = [int(c) for c in self.__COLORS[classIDs[i]]]
+                cv2.rectangle(out_frame, (x, y), (x + w, y + h), color, 2)
 
-                    # use the center (x, y)-coordinates to derive the top
-                    # and and left corner of the bounding box
-                    x = int(centerX - (width / 2))
-                    y = int(centerY - (height / 2))
-                    # update our list of bounding box coordinates,
-                    # confidences, and class IDs
-                    boxes.append([x, y, int(width), int(height)])
-                    confidences.append(float(confidence))
-                    classIDs.append(classID)
-
-        # apply non-maxima suppression to suppress weak, overlapping
-        # bounding boxes
-
+            cv2.imwrite("yolov4_detection_without_nms.png", out_frame)
         idxs = cv2.dnn.NMSBoxes(boxes, confidences, self.__confidence_setting, self.__threshold)
 
         # ensure at least one detection exists
@@ -151,43 +134,7 @@ class yolo_object_detection():
                     cv2.rectangle(frame, (x, y), (x + w, y + h), color, 2)
                     text = "{}: {:.4f}".format(self.__LABELS[classIDs[i]], confidences[i])
                     cv2.putText(frame, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
-        print(boxes)
-        cv2.imwrite("frame.jpg", frame)
+        cv2.imwrite("yolov4_detection_with_nms.jpg", frame)
         return boxes
 
-    def IOU_check_for_first_frame(self,src_bbox, adjust_bboxes):
-        print("IoU method(first_frame)")
-        iou_temp = []
-        boxSRCArea = (src_bbox[2] + 1) * (src_bbox[3] + 1)
-        for i, adjust_bbox in enumerate(adjust_bboxes):
-            xA = max(src_bbox[0], adjust_bbox[0])
-            # print("xA:%.2f" % xA)
-            yA = max(src_bbox[1], adjust_bbox[1])
-            # print("yA:%.2f" % yA)
-            xB = min(src_bbox[0] + src_bbox[2], adjust_bbox[0] + adjust_bbox[2])
-            # print("xB:%.2f" % xB)
-            yB = min(src_bbox[1] + src_bbox[3], adjust_bbox[1] + adjust_bbox[3])
-            # print("yB:%.2f" % yB)
 
-            interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
-            # print("interArea:%.2f" % interArea)
-
-            boxADJArea = (adjust_bbox[2] + 1) * (adjust_bbox[3] + 1)
-            # compute the intersection over union by taking the intersection
-            # area and dividing it by the sum of prediction  ground-truth
-            # areas - the intersection area
-            iou = interArea / float(boxSRCArea + boxADJArea - interArea)
-            iou_temp.append(iou)
-
-        # if max(iou_temp) > 0:
-        iou_array = np.array(iou_temp)
-        index = np.argmax(iou_array)
-        print("iou_array:")
-        print(iou_array)
-
-        if iou_array[index] > 0.5:
-            print("A")
-            return adjust_bboxes[index]
-        else:
-            print("B")
-            return src_bbox
