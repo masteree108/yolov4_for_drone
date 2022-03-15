576827abf1a2d53fcf06b44c6d3aa3ab4deab24e
DNS-0522 <xkoukix111@gmail.com>
2022-03-16 01:17:05 +0800

    show diff detect intensity

--- yolo_test.py
+++ yolo_test.py
@@ -3,16 +3,21 @@ import cv2
 from imutils.video import FPS
 
 if __name__ == '__main__':
-    img = cv2.imread('./image/047.jpg')
-
+    img = cv2.imread('./image/New-York_1280x1024.jpg')
     fps = FPS().start()
     yolo = yolo_obj.yolo_object_detection('person')
     one_core_to_run = True
     if one_core_to_run == True:
         final_bboxes = []
-        detection_intensity=1
-        yolo_boxes = yolo.run_detection(img,detection_intensity)
-        print("detected person qty:%d"%len(yolo_boxes))
+        detection_intensity = 2
+        yolo_boxes = yolo.run_detection(img, detection_intensity)
+        if detection_intensity == 0:
+            print("weak")
+        elif detection_intensity == 1:
+            print("medium")
+        elif detection_intensity == 2:
+            print("strong")
+        print("detected person qty:%d" % len(yolo_boxes))
     else:
         yolo.run_multi_core_detection_setting(img)
         yolo.run_multi_core_detection(img)
